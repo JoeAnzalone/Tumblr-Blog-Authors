c8cec1bd157e9975551969191764ccdf047542ce
Joe Anzalone <Joe@Shmit.com>
2014-04-20 16:17:19 -0400

    Loop through all pages of a blog instead of stopping at the first to get authors

--- app/models/Blog.php
+++ app/models/Blog.php
@@ -2,16 +2,44 @@
 
 class Blog extends Eloquent {
 
+    public $postsPerPage = 20;
+
     public function __construct($blog_name) {
         $this->blog_name = $blog_name;
+        $this->client    = $client = new Tumblr\API\Client(
+                                        Config::get('tumblr.consumer_key')
+                                    );
+    }
+
+    private function _totalPosts() {
+        $response = $this->client->getBlogInfo($this->blog_name);
+        return $response->blog->posts;
+    }
+
+    private function _authorsByOffset($offset) {
+        $options = array('offset' => $offset);
+        $response = $this->client->getBlogPosts($this->blog_name, $options);
+        $response->total_posts;
+
+        foreach ($response->posts as $post) {
+            $authors[] = $post->post_author;
+        }
+        return $authors;
     }
 
     public function authors() {
-        $client = new Tumblr\API\Client(Config::get('tumblr.consumer_key'));
-        $posts = $client->getBlogPosts($this->blog_name)->posts;
-        foreach ($posts as $post) {
-            $authors[] = $post->post_author;
+        $total_posts = $this->_totalPosts();
+        $pages = round($total_posts / $this->postsPerPage);
+
+        $authors = array();
+
+        for ($i=0; $i < $pages; $i++) {
+            $add_authors = $this->_authorsByOffset($i * $this->postsPerPage);
+            $authors = array_merge($authors, $add_authors);
         }
+
+        $authors = array_unique($authors);
+        $authors = array_values($authors);
 
         return $authors;
     }
