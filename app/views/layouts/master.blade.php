58758e87689cb7c1136220eb5178d6a02cef30a7
Joe Anzalone <Joe@Shmit.com>
2014-04-20 20:54:43 -0400

    Add new HTML comment signature to bottom of default layout

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -12,3 +12,20 @@
 </div>
 </body>
 </html>
+<!--
+                       88
+                       88
+                       88
+                       88   ,adPPYba,    ,adPPYba,
+                       88  a8"     "8a  a8P_____88
+                       88  8b       d8  8PP"""""""
+       db      88,   ,d88  "8a,   ,a8"  "8b,   ,aa    88
+      d88b      "Y8888P"    `"YbbdP"'    `"Ybbd8"'    88
+     d8'`8b                                           88
+    d8'  `8b      8b,dPPYba,   888888888  ,adPPYYba,  88   ,adPPYba,   8b,dPPYba,    ,adPPYba,
+   d8YaaaaY8b     88P'   `"8a       a8P"  ""     `Y8  88  a8"     "8a  88P'   `"8a  a8P_____88
+  d8""""""""8b    88       88    ,d8P'    ,adPPPPP88  88  8b       d8  88       88  8PP"""""""
+ d8'        `8b   88       88  ,d8"       88,    ,88  88  "8a,   ,a8"  88       88  "8b,   ,aa
+d8'          `8b  88       88  888888888  `"8bbdP"Y8  88   `"YbbdP"'   88       88   `"Ybbd8"'
+
+This has been a JoeAnzalone.com Production -->
