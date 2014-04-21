1c9b9b44a9223c481859b8f0fe129da8b27ded53
Joe Anzalone <Joe@Shmit.com>
2014-04-20 21:57:02 -0400

    Style homepage form and add instructions

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -1,4 +1,12 @@
+<h1>Tumblr Blog Authors</h1>
+
 <form action="." method="get">
-    <input type="text" name="blog">
+    <input type="text" name="blog" placeholder="engineering" autofocus required>
     <button type="submit">Go</button>
 </form>
+
+<p>Just enter in the Tumblr username of a group blog and this tool will generate a list of all Tumblr users that have ever posted to that blog.</p>
+
+<p>For example, you can try it with <a href="/engineering">engineering</a> or <a href="/ministryofdesign">ministryofdesign</a>.</p>
+
+<p>This thing was created by Tumblr user <a href="http://HTMLbyJoe.tumblr.com" target="_blank">HTMLbyJoe</a> and its source is <a href="https://github.com/JoeAnzalone/Tumblr-Blog-Authors" target="_blank">available on GitHub</a>.</p>
